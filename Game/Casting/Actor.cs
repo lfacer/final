1d8ffc8a17615ae29728b608fc78bcf4332cb2e7
lilsoren <lily.m.sorensen@gmail.com>
2022-07-14 19:45:25 -0600

    moving background

--- Game/Casting/Actor.cs
+++ Game/Casting/Actor.cs
@@ -15,8 +15,7 @@ namespace Unit06.Game.Casting
         private bool debug;
         private float _scale = 1f;
         private Color _tint = Color.White();
-        private Point _velocity_b = new Point(0, 0);
-        private Point _position_b = new Point(0, 0);
+       
         
         /// <summary>
         /// Constructs a new instance of Actor.
@@ -143,35 +142,7 @@ namespace Unit06.Game.Casting
             return _velocity;
         }
 
-        // gets the backgrounds position
-        public Point GetPositionB()
-        {
-            return _position_b;
-        }
-
-        // moves the background according to its next position according to its velocity. will wrap the position from one side of the screen to the other 
-        public void MoveNextB(int maxX, int maxY)
-        {
-            int x = ((_position_b.GetX() + _velocity_b.GetX()) + maxX) % maxX;
-            int y = ((_position_b.GetY() + _velocity_b.GetY()) + maxY) % maxY;
-            _position_b = new Point(x, y);
-        }
-
-        // sets the backgrounds velocity to the given value
-        public void SetVelocityB(Point _velocity_b)
-        {
-            if (_velocity_b == null)
-            {
-                throw new ArgumentException("velocity can't be null");
-            }
-            this._velocity_b = _velocity_b;
-        }
-
-
-        public Point GetVelocityB()
-        {
-            return _velocity_b;
-        }
+        
 
         public virtual float GetWidth()
         {
